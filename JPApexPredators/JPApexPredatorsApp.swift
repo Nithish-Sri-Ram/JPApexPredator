1c8d2e439cc375655cb942cd499206557c63834f
Nithish <nithishsriram_tt@srmap.edu.in>
2025-05-19 03:30:25 +0530

    First commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Nithish on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
